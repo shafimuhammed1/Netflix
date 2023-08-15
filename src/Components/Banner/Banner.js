f30a42615531f250022727972641a5c7540fd68a
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-08-15 17:16:27 +0530

    Navbar designed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='Movie-title'>Movie Name</h1>
+            <div className='movie-buttons'>
+                <button className='buttons'>Play</button>
+                <button className='buttons'>My list</button>
+            </div>
+            <h1 className='description'>
+            In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the
+            </h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Banner
