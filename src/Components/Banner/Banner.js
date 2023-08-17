8780a699bd78d314f84f2b1e64ee433b7367e97e
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-08-17 23:41:54 +0530

    installed axios and integrated to banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY} from '../../Constants/constants'
 
 function Banner() {
+  useEffect(()=>{
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+    })
+
+
+  },[])
   return (
     <div className='banner'>
         <div className='content'>
---
+++ src/Constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'b04be5892ee335235f5f29f64c36ebbc';
