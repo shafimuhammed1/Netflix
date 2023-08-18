d37d835052ce5e4f1f7e21df861b35e87fc8a60f
Muhammed Shafi <mohdshafi13112000@gmail.com>
2023-08-18 23:00:50 +0530

    add title description and image in banner by using state

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,26 +1,28 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Banner.css'
 import axios from '../../axios'
-import {API_KEY} from '../../Constants/constants'
+import {API_KEY,imgUrl} from '../../Constants/constants'
 
 function Banner() {
+  const [movie,setMovie]=useState()
   useEffect(()=>{
     axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
       console.log(response.data.results[0])
+      setMovie(response.data.results[0])
     })
 
 
   },[])
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie?imgUrl+movie.backdrop_path:''})`}}  className='banner'>
         <div className='content'>
-            <h1 className='Movie-title'>Movie Name</h1>
+            <h1 className='Movie-title'>{movie ? movie.title:""}</h1>
             <div className='movie-buttons'>
                 <button className='buttons'>Play</button>
                 <button className='buttons'>My list</button>
             </div>
             <h1 className='description'>
-            In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the
+            {movie ? movie.overview:''}
             </h1>
         </div>
         <div className="fade-bottom"></div>
--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 export const API_KEY = 'b04be5892ee335235f5f29f64c36ebbc';
+export const imgUrl = 'https://image.tmdb.org/t/p/original';
